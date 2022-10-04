6fed18cc652539eb766664214a71f55c325dadf4
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2022-10-04 20:07:26 +0200

    feat: create components and service for auth

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private fireAuth: AngularFireAuth, private router: Router) {}
+}
